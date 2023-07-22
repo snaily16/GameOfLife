1ddb90f46452b19050f4e19e697b62ad8dce0589
SNEHAL GHARAT <gharatsnehal.16@gmail.com>
2023-07-22 23:41:49 +0530

    Code changes
    Added patterns and removed redundant code

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 // Conway's Game of Life
 // Snehal Gharat
-
 function make2DArray(rows, cols){
   let arr = new Array(rows);
   for(let i=0; i<rows; i++){
@@ -17,34 +16,40 @@ var toggleButton;
 var sel, pat_sel;
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(1200, 500);
   frameRate(20);
   rows = width / resolution;
   cols = height / resolution;
-  //console.log(rows, cols)
-  //randomize();
   generateGosperGlider();
-  
-  //var randomButton = createButton("Randomize");
-  //var patternButton = createButton("Patterns");
   toggleButton = createButton("Pause");
+  enableMouseDrag = createButton("Enable Mouse Drag");
   var blankButton = createButton("Blank Screen");
+  enable_mouse_drag = false;
+  enableMouseDrag.mousePressed(function(){
+    if(enable_mouse_drag){
+      enableMouseDrag.html('Enable Mouse Drag');
+      enable_mouse_drag=false;
+    }
+    else{
+      enableMouseDrag.html('Disable Mouse Drag');
+      enable_mouse_drag=true;
+    }
+  });
+  drawpatterncenterbtn = createButton("Draw Pattern At Center");
+  drawpatterncenterbtn.mousePressed(function(){drawPattern(54,40)});
   
   sel = createSelect();
   //sel.position(10, 10);
   sel.option('Random')
   sel.option('Gosper-Glider-Gun')
   sel.option('r-pentomino');
-  sel.option('Glider');
   sel.selected('Gosper-Glider-Gun');
   sel.changed(mySelectPattern);
   
   patSel = createSelect();
-  patSel.option('Block')
-  patSel.option('Beehive');
-  patSel.option('Boat');
-  patSel.option('Blinker');
-  patSel.option('Toad');
+  options = ['Block', 'Beehive', 'Beehive2', 'HoneyComb', 'Boat', 'Blinker', 'Toad', 'QueenBee', 'HoneyFarm', 'SpaceShip1', 'Glider', 'Glider-Gun', 'Heart', 'r-pentomino'];
+  for(var i=0; i<options.length; i++)
+    patSel.option(options[i]);
   patSel.selected('Block');
   patSel.changed(drawPattern);
   
@@ -61,9 +66,6 @@ function mySelectPattern(){
   if(item == 'r-pentomino'){
     generateRPentomino();
   }
-  if(item == 'Glider'){
-    generateGlider();
-  }
   if(item == 'Gosper-Glider-Gun'){
     generateGosperGlider();
   }
@@ -78,38 +80,39 @@ function randomize(){
   }
 }
 
-function generateRPentomino(){
+// create blank 2d array
+function blankSketch(){
   grid =  make2DArray(rows, cols);
   for(let i=0; i<rows; i++){
     for(let j=0; j<cols; j++){
       grid[i][j] = 0;
     }
   }
-  
-  x_indices = [0, 1, 0, -1, 0]
-  y_indices = [0, 0, 1, 1, 2]
+  return grid;
+}
+
+function generateRPentomino(){
+  grid = blankSketch();
+  indices = GetPatterns('r-pentomino');
+  x_indices = indices[0];
+  y_indices = indices[1];
   var a=60;
   var b=40;
   
-  for(var i=0;i<5;i++){
+  for(var i=0;i<x_indices.length;i++){
     grid[a+x_indices[i]][b+y_indices[i]]=1;
   }
   //noLoop();
 }
 
 function generateGosperGlider(){
-  grid =  make2DArray(rows, cols);
-  for(let i=0; i<rows; i++){
-    for(let j=0; j<cols; j++){
-      grid[i][j] = 0;
-    }
-  }
+  grid = blankSketch();
   var a=10;
   var b=10;
   
-  
-  x_indices=[24,22,24,12,13,20,21,34,35,11,15,20,21,34,35,0,1,10,16,20,21,0,1,10,14,16,17,22,24,10,16,24,11,15,12,13];
-  y_indices=[0,1,1,2,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,4,4,5,5,5,5,5,5,5,5,6,6,6,7,7,8,8];
+  indices = GetPatterns('Glider-Gun');
+  x_indices = indices[0];
+  y_indices = indices[1];
   for(var i=0;i<36;i++){
     grid[a+x_indices[i]][b+y_indices[i]]=1;
   }
@@ -128,35 +131,8 @@ function generateGosperGlider(){
   for(var i=0;i<36;i++){
     grid[a-x_indices[i]][b-y_indices[i]]=1;
   }
-  
-  //noLoop();
 }
 
-function generateGlider(){
-  grid =  make2DArray(rows, cols);
-  for(let i=0; i<rows; i++){
-    for(let j=0; j<cols; j++){
-      grid[i][j] = 0;
-    }
-  }
-  var a=60;
-  var b=40;
-  grid[a][b]=1;
-  grid[a+1][b+1]=1;
-  grid[a+2][b+1]=1;
-  grid[a][b+2]=1;
-  grid[a+1][b+2]=1;
-  //noLoop();
-}
-
-function blankSketch(){
-  grid =  make2DArray(rows, cols);
-  for(let i=0; i<rows; i++){
-    for(let j=0; j<cols; j++){
-      grid[i][j] = 0;
-    }
-  }
-}
 
 function playSketch(){
   if(isLooping()){
@@ -170,25 +146,20 @@ function playSketch(){
 }
 
 function countNeighbours(grid, x,y){
-  let sum = 0;
+  let alivenbrs = 0;
   for(let i=-1; i<2; i++){
     for(let j=-1; j<2; j++){
       if(i==0 && j==0){
         continue;
       }
-      let live = 0;
       let rx = ((x+i)%rows + rows)%rows;
-      let ry = ((y+j)%cols + cols)%cols;
-      
-      //let val_rows = !(x+i < 0 || x+i >= rows)
-      //let val_cols = !(y+j < 0 || y+j >= cols)
-      //if (val_rows && val_cols){
-        if (grid[rx][ry]>=1){live = 1;}
-        sum = sum + live;
-      //}
+      let ry = ((y+j)%cols + cols)%cols;    
+    
+      if (grid[rx][ry]>=1){alivenbrs = alivenbrs + 1;}
+
     }
   }
-  return sum;
+  return alivenbrs;
 }
 
 function updateCell(i,j){
@@ -210,81 +181,77 @@ function updateCell(i,j){
   return new_state;
 }
 
-function drawPattern(){
+function drawPattern(x=0,y=0){
   let pattern=patSel.value();
-  let xp = ~~(mouseX / resolution);
-  let yp = ~~(mouseY / resolution);
+  if (x!=0) xp=x;
+    else xp = ~~(mouseX / resolution);
+  if(y!=0) yp=y;
+    else yp = ~~(mouseY / resolution);
   
-  if(pattern=="Block"){
-    grid[xp][yp]=1;
-    if(xp+1<rows)
-        grid[xp+1][yp]=1; 
-    if(yp+1<cols)
-        grid[xp][yp+1]=1; 
-  }
-  else if(pattern=="Beehive"){
-    x_indices=[0,0,1,1,2,2]
-    y_indices=[0,2,-1,2,0,1]
-    for(var i=0;i<6;i++){
-    grid[xp+x_indices[i]][yp+y_indices[i]]=1;
-  }
-  }
-  else if(pattern=="Blinker"){
-    grid[xp][yp]=1;
-    grid[xp+1][yp]=1;
-    grid[xp+2][yp]=1;
-  }
-  else if(pattern=="Boat"){
-    grid[xp][yp]=1;
-    grid[xp+1][yp]=1;
-    grid[xp][yp+2]=1;
-  }
-   else if(pattern=="Toad"){
-    x_indices=[0,1,2,0,0,0]
-    y_indices=[0,0,0,1,2,3]
-    for(var i=0;i<6;i++){
-    grid[xp+x_indices[i]][yp+y_indices[i]]=1;
-    }
-  }
+  indices = GetPatterns(pattern);
+  x_indices = indices[0];
+  y_indices = indices[1];
+
+  for(let i=0;i<x_indices.length;i++)
+     grid[xp+x_indices[i]][yp+y_indices[i]]=1;
 }
 
 function draw() {
   background(0);
+  colorMode(HSB,360,100,100); // HSB colors with max hue 360, max saturation 100 and max brightness 100
+  // https://p5js.org/reference/#/p5/colorMode
+  var h1=20,s1=0,b1=100; // start at white
+  var h2=40,s2=100,b2=100; // orange-ish
+  var ht,st,bt;
+  var max_num_generations=15;
+  
+  strokeWeight(0.1);
   for(let i=0; i<rows; i++){
     for(let j=0; j<cols; j++){
       
       let x = i * resolution;
       let y = j * resolution;
-      if(mouseIsPressed == true){
+      if(mouseIsPressed == true && enable_mouse_drag == true){
        drawPattern();
       }
       if (grid[i][j]>=1){
         var c  = grid[i][j]*2;
-        fill(255-c,255-c,255+c);
-        stroke(0);
-        rect(x,y,resolution-1, resolution-1);
+        var t = grid[i][j]/max_num_generations;
+        if(t>1)t=1;
+        ht=t*h2+(1-t)*h1; // convex combination on HSB space
+        st=t*s2+(1-t)*s1; // results in smooth colors
+        bt=t*b2+(1-t)*b1; // between color1 and color2
+        fill(ht,st,bt);
+        stroke(ht,st,bt);
+        //rect(x,y,resolution-1, resolution-1);
+        circle(x,y,resolution);
       }
     }
   }
   
  gameOfLife();
 }
-
+function mousePressed(){
+  if (enable_mouse_drag == false){
+    drawPattern();
+  }
+}
 function gameOfLife(){
    let temp_curr;
   let temp_prev;
-  //let new_grid = make2DArray(rows, cols);
+    let temp_0;
   for(let i=0; i<rows; i++){
     temp_curr = new Array(cols).fill(0);
     for(let j=0; j<cols; j++){
       temp_curr[j] = updateCell(i,j);
-      //new_grid[i][j] = updateCell(i,j);
     }
-    if(i>0){
+    if(i>1){
       grid[i-1] = temp_prev;
     }
     temp_prev = temp_curr;
+    if(i==0)
+      temp_0 = temp_curr;
   }
   grid[rows-1] = temp_prev;
-  //grid = new_grid;
+  grid[0] = temp_0;
 }
